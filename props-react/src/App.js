bdbd4cd0f895f7367b5e7d478c3ce3409984e307
LocTM <tranminhloc.anhk@gmail.com>
2024-04-03 22:52:57 +0700

    first-cm

---
+++ props-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Welcome} from "./Welcome";
+
+function App() {
+  return (
+   <>
+     <Welcome name={"ADMIN"}/>
+   </>
+  );
+}
+
+export default App;
