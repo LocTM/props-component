129890cac9465efe9123ae87ec52c372fcfd3fbe
LocTM <tranminhloc.anhk@gmail.com>
2024-04-03 23:03:13 +0700

    add boostrap, router

--- props-react/src/App.js
+++ props-react/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import {Welcome} from "./Welcome";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <>
      <Welcome name={"ADMIN"}/>
+       <div className="container">
+           <h2>React CRUD Tutorial</h2>
+       </div>
    </>
   );
 }
